57b7216ed9b44c9aeec865419b15e6b1970aa894
marcoalsina <47423950+marcoalsina@users.noreply.github.com>
2019-10-11 11:56:59 -0300

    Create __init__.py

---
+++ pyxas/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from .main import index_dups, DataReport
+from .main import read_dnd, calibrate_energy, align_scans, merge_scans, get_scan_type
+from .main import xftf_pha
+
+__version__ = '0.1.1'
