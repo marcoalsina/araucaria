3a49d3f0e547ce45dd1de28f774a74ecd1cf6cdc
marcoalsina <47423950+marcoalsina@users.noreply.github.com>
2019-10-27 18:20:12 -0300

    Update __init__.py

--- pyxas/fit/__init__.py
+++ pyxas/fit/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from .lcf import lcf
-from .utils import sum_standards, residuals, lcf_report, get_lcf_data, get_chi2
+from .utils import sum_references, residuals, lcf_report, get_lcf_data, get_chi2
