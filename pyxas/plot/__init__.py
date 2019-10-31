26865ce2936815ec24aed8978e2b5d857c44059d
marcoalsina <47423950+marcoalsina@users.noreply.github.com>
2019-10-31 18:02:28 -0300

    Update __init__.py

--- pyxas/plot/__init__.py
+++ pyxas/plot/__init__.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python
-from .plot import fig_xas_template, plot_merged_scans, fig_lcf
+from .plot import fig_xas_template, plot_merged_scans
+from .fig_lcf import fig_lcf
+from .fig_lsf import fig_lsf
