b09a70d8e45a787a60c1999ec02c53cc4906399b
marcoalsina <47423950+marcoalsina@users.noreply.github.com>
2019-12-19 11:36:30 -0300

    Update __init__.py

--- pyxas/__init__.py
+++ pyxas/__init__.py
@@ -4,4 +4,9 @@ from .main import index_dups, get_scan_type, DataReport
 from .main import xftf_pha
 from .merge import calibrate_energy, align_scans, merge_scans, merge_spectra, merge_report
 
-__version__ = '0.1.1'
+# reading file version
+f   = open(os.path.join('..','version'), 'r')
+ver = f.readline()
+f.close()
+
+__version__ = ver
