7becbe791750c1e81d42bd376eb4d2cc581cf777
marcoalsina <marco.alsina@utalca.cl>
2019-12-19 17:09:00 -0300

    Update version.

--- pyxas/__init__.py
+++ pyxas/__init__.py
@@ -6,7 +6,7 @@ from .main import xftf_pha
 from .merge import calibrate_energy, align_scans, merge_scans, merge_spectra, merge_report
 
 # reading file version
-f   = open(os.path.join('..','version'), 'r')
+f   = open(os.path.join(os.path.dirname(__file__), '..', 'version'), 'r')
 ver = f.readline()
 f.close()
 
