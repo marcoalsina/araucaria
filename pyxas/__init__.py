32735a9d816d70ca282d97172eee2ede9a7b80a9
marcoalsina <marco.alsina@utalca.cl>
2019-12-19 12:37:50 -0300

    Update version path.

--- pyxas/__init__.py
+++ pyxas/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
 from .main import index_dups, get_scan_type, DataReport
 from .main import xftf_pha
 from .merge import calibrate_energy, align_scans, merge_scans, merge_spectra, merge_report
 
-__version__ = '0.1.1'
+# reading file version
+f   = open(os.path.join(os.path.dirname(__file__), '..', 'version'), 'r')
+ver = f.readline()
+f.close()
+
+__version__ = ver
