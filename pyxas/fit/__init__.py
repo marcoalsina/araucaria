ad238538cd0522fc2c99a7f36ba1c03bfa371f04
marcoalsina <marco.alsina@utalca.cl>
2019-12-19 14:42:38 -0300

    Adding ecf routines.

--- pyxas/fit/__init__.py
+++ pyxas/fit/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from .lcf import lcf
+from .lcf import lcf, sum_references, residuals
 from .lsf import lsf
-from .utils import sum_references, residuals, lcf_report
+from .ecf import ecf, ecf_minfunc
 from .utils import get_lcf_data, get_chi2
 from .utils import lcf_report, save_lcf_report, save_lcf_data
 from .utils import lsf_report, save_lsf_report, save_lsf_data
