3d24552f20adfc5942e323e1784c0c76608dee73
marcoalsina <47423950+marcoalsina@users.noreply.github.com>
2020-09-07 22:30:01 -0300

    Update __init__.py

--- pyxas/__init__.py
+++ pyxas/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-from .main import index_dups, get_scan_type, DataReport
+from .main import Group, DataReport, index_dups, get_scan_type
 from .main import xftf_pha
 from .merge import calibrate_energy, align_scans, merge_scans, merge_spectra, merge_report
 
