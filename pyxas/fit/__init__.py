c28af04080c12ce41ff7ba506c9290e5298c271c
marcoalsina <marco@localhost.localdomain>
2020-11-02 12:19:12 -0300

    deleting previous files

--- pyxas/fit/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from .lcf import lcf, sum_references, residuals
-from .lsf import lsf
-from .ecf import ecf, ecf_minfunc
-from .utils import get_lcf_data, get_chi2
-from .utils import fit_report, save_fit_report, save_fit_data
-from .utils import lsf_report, save_lsf_report, save_lsf_data
--- pyxas/io/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from .io_hdf5 import read_hdf5, write_hdf5, write_recursive_hdf5
-from .io_hdf5 import rename_dataset_hdf5, delete_dataset_hdf5
-from .io_hdf5 import summary_dataset_hdf5, summary_scans_dataset_hdf5
-from .io_files import read_p65, read_dnd, read_xmu
