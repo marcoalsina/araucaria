01e63557ced496798983da2c717a3dffc2384414
marcoalsina <47423950+marcoalsina@users.noreply.github.com>
2020-11-02 19:57:47 -0300

    Add files via upload

---
+++ docs/build/html/plot_module-1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+from araucaria.plot import fig_xas_template
+pars = {'e_range' : (0,100),
+        'mu_range': (0,1.5),
+        'k_range' : (0,15),
+        'r_range' : (0,6)}
+fig, axes = fig_xas_template('dx/er', pars=pars)
+fig.tight_layout()
+plt.show(block=False)
